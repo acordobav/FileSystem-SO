a60a1dd2c1e88a1d58c8f00f80fb92ad2fbfc9fe
Arturo Cordoba <arturocv16@gmail.com>
2020-12-10 22:49:11 -0600

    Main

---
+++ Logic/main.c
@@ -0,0 +1,55 @@
+#define __USE_XOPEN 700
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "filesystem.c"
+
+int main(void) {
+    /*
+    startFileSystem(32, 100000);
+    //printf("%s\n", tree->root->kid->brother->kid->filedata->name);
+    //printf("%s\n", root->kid->brother->kid->filedata->name);
+    
+    //printf("%s\n", tree->root->kid->brother->kid->filedata->name);
+    void* file1 = search(tree->root, "file1.txt");
+    //printf("%p\n", file1);
+    char* read = readFile(file1);
+    printf("%s\n", read);
+
+    for (int i = 0; i < 15; i++)
+    {
+        printf("%d ",usedBlocks[i]);
+    }*/
+    
+    
+    
+    //*/
+    
+    
+    
+    /*
+    startFileSystem(32,10000);
+    
+    Node* file1 = touch(tree->root, "file1.txt", "Arthur-Satellite");
+    char* msg = "Mensaje secreto ultraimportante para el gobierno costarricense";
+    writeFile(file1, msg);
+    
+    mkdir(tree->root, "TEC", "Rogers");
+    void* ref = search(tree->root, "TEC");
+    void* file2 = touch(ref, "Algebra", "Rogers");
+    writeFile(file2, msg);
+
+    void* file3 = touch(tree->root, "Examen 2.pdf", "Arthur");
+    writeFile(file3, msg);*/
+
+    //tree_to_json(tree, DISKMETADATA);
+    //*/
+
+
+
+    return 0;
+}
